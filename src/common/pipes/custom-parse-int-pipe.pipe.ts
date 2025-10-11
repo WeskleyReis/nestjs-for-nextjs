1ae65957fb9bff921681ff99c6f697b908b36634
WeskleyReis <weskleyff88004216@gmail.com>
2025-10-11 13:04:20 -0300

    Iniciando integração com banco de dados local.

---
+++ src/common/pipes/custom-parse-int-pipe.pipe.ts
@@ -0,0 +1,9 @@
+import { BadRequestException, ParseIntPipe } from "@nestjs/common"
+
+export class CustomParseIntPipe extends ParseIntPipe {
+  constructor() {
+    super({
+      exceptionFactory: () => new BadRequestException('Parâmetro precisa ser um numero')
+    })
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Get(':id')
-  findOne(@Param('id') id:string) {
+  findOne(@Param('id', CustomParseIntPipe) id: number) {
     return `Olá do controller do user #${id}`
   }
 }
